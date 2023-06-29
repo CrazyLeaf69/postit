f6c107096bcefe99defe6103d1a85250195bce7b
Lowe Löwing <lowe.lowing@gmail.com>
2023-06-29 17:38:49 +0200

    updated logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,22 +6,36 @@ import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: "Breadit",
+  title: "Postit",
   description: "A Reddit clone built with Next.js and TypeScript.",
 };
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children, authModal }: { children: React.ReactNode; authModal: React.ReactNode }) {
+export default function RootLayout({
+  children,
+  authModal,
+}: {
+  children: React.ReactNode;
+  authModal: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={cn("bg-white text-slate-900 antialiased font-light", inter.className)}>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased font-light",
+        inter.className
+      )}
+    >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
         <Providers>
           <Toaster />
           {/* @ts-expect-error server component */}
           <Navbar />
           {authModal}
-          <div className="container max-w-7xl mx-auto h-full pt-12">{children}</div>
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
         </Providers>
       </body>
     </html>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,25 @@
 import { getAuthSession } from "@/lib/auth";
 import Link from "next/link";
-import { Icons } from "./Icons";
 import SearchBar from "./SearchBar";
 import UserAccountNav from "./UserAccountNav";
 import { buttonVariants } from "./ui/Button";
+import logo from "@/assets/icon.svg";
+import Image from "next/image";
 
 const Navbar = async () => {
   const session = await getAuthSession();
 
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-      <div className="container max-w-7xl-h-full mx-auto flex items-center justify-between gap-2">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
         <Link href="/" className="flex gap-2 items-center">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+          <Image
+            src={logo}
+            alt="logo"
+            width={32}
+            height={32}
+            className="max-w-[32px] h-8"
+          />
           <p className="hidden text-zinc-700 text-sm font-medium md:block">
             Postit
           </p>
